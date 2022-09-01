308cbc93b60c0b9be9f503b3c87ccaec4a77e406
Abhishek-1705 <am9893073458@gmail.com>
2022-09-02 02:14:23 +0530

    added navbar and WTB member section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,16 @@
+/* eslint-disable react/jsx-pascal-case */
 import React, { Component } from 'react'
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Landing from '../Component/Landing';
+// import  NavBarcomp  from '../Component/NavBarcomp';
+import { WTB_Member }  from '../Component/WTB_Member';
 export default class Home extends Component {
   render() {
     return (
-      <div className='container d-flex justify-content-center align-items-center'>
-        <Landing/>
-
+      <div className='d-flex flex-column' >
+        {/* <NavBarcomp /> */}
+        <Landing />
+        <WTB_Member />
       </div>
     )
   }

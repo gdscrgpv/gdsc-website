344bbe2be93cac37d620bb45ee6c7b777c4f5bfe
Abhishek-1705 <am9893073458@gmail.com>
2022-09-03 02:34:57 +0530

    made changes to Partners section  & fix the Responsive  of section WTB_member , About and Partners

--- src/Component/AboutUs.js
+++ src/Component/AboutUs.js
@@ -1,83 +1,85 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./AboutUsStyle.css";
 import gsap from "gsap";
-import Img from '../Assets/img/AboutUs.png';
+import Img from "../Assets/img/AboutUs.png";
 import Transition from "./Transition/Transition";
 
 const AboutUs = () => {
+  const [hid, setHid] = useState(false);
 
-    const [hid, setHid] = useState(false);
+  const aboutHeading = useRef(null);
+  const aboutImg = useRef(null);
+  useEffect(() => {
+    // aboutUs.from(aboutHeading, {
+    //     duration: .6,
+    //     skewX: 10,
+    //     x: -100,
+    //     opacity: 0
+    // }, 3.5)
+    // aboutUs.from(aboutImg, {
+    //     duration: .5,
+    //     y: -200,
+    //     opacity: 0
+    // }, 3)
+  });
 
-    const aboutHeading = useRef(null);
-    const aboutImg = useRef(null);
-    useEffect(() => {
-        // aboutUs.from(aboutHeading, {
-        //     duration: .6,
-        //     skewX: 10,
-        //     x: -100,
-        //     opacity: 0
-        // }, 3.5)
-        // aboutUs.from(aboutImg, {
-        //     duration: .5,
-        //     y: -200,
-        //     opacity: 0
-        // }, 3)
-    })
+  setTimeout(() => {
+    setHid(true);
+  }, 600);
 
-    setTimeout(() => {
-        setHid(true)
-    }, 600);
+  return (
+    <>
+      <div className="mainView">
+        <div className="leftBox">
+          {/* heading section */}
 
-   
-    return (
-        <>
+          {/* yellow line */}
+          <div className="line space"></div>
 
-            <div className="mainView flex">
+          <div className="flex top" ref={aboutHeading}>
+            <h1 className="space"> About</h1>
 
-                
-                <div className="leftBox">
-                    {/* heading section */}
-
-                    {/* yellow line */}
-                    <div className="line space"></div>
-
-                    <div className="flex top" ref={aboutHeading}>
-
-                        <h1 className="space"> About</h1>
-
-                        <div className="flex gdsc">
-                            <span className=" G bgDiv"><h1>G</h1></span>
-                            <span className=" D bgDiv"><h1>D</h1></span>
-                            <span className=" S bgDiv"><h1>S</h1></span>
-                            <span className=" C bgDiv"><h1>C</h1></span>
-                        </div>
-                    </div>
-
-                    <div className="flex">
-                        <div className="line space"></div>
-                        <div className="smallLine space"></div>
-                    </div>
-
-
-                    {/* content section */}
-                    <div className="content textbox" >
-
-                        {/* text  */}
-                        <h5 className="font-face-lighter ">
-                            Google Student Developer Clubs (GDSC) is a student-led network that allows undergrad students to upgrade their skills and learn about various domains of computing. It gives students the potential to expand their technological skills, create solutions for their local communities, and network with other members of the Google community.
-                        </h5>
-
-                    </div>
-                </div>
-
-
-                {/* image */}
-                <div className="imageDiv" ref={aboutImg}>
-                    <img className="image" src={Img} />
-                </div>
+            <div className="flex gdsc">
+              <span className=" G bgDiv">
+                <h1>G</h1>
+              </span>
+              <span className=" D bgDiv">
+                <h1>D</h1>
+              </span>
+              <span className=" S bgDiv">
+                <h1>S</h1>
+              </span>
+              <span className=" C bgDiv">
+                <h1>C</h1>
+              </span>
             </div>
+          </div>
 
-        </>
-    )
-}
+          <div className="flex">
+            <div className="line space"></div>
+            <div className="smallLine space"></div>
+          </div>
+
+          {/* content section */}
+          <div className="content textbox">
+            {/* text  */}
+            <h5 className="font-face-lighter ">
+              Google Student Developer Clubs (GDSC) is a student-led network
+              that allows undergrad students to upgrade their skills and learn
+              about various domains of computing. It gives students the
+              potential to expand their technological skills, create solutions
+              for their local communities, and network with other members of the
+              Google community.
+            </h5>
+          </div>
+        </div>
+
+        {/* image */}
+        <div className="imageDiv" ref={aboutImg}>
+          <img className="image" src={Img} />
+        </div>
+      </div>
+    </>
+  );
+};
 export default AboutUs;
--- src/Component/Partners.js
+++ src/Component/Partners.js
@@ -8,27 +8,44 @@ export default class Partners extends Component {
     return (
       <div className="partners">
         <div className="partners-heading font-face-bold flex">
-            <h2 className="text-white margin-10px">Partners at</h2>
-            <h2 className="G margin-10px">GDSC</h2>
-            <h2 className="D margin-10px">UIT</h2>
-            <h2 className="S margin-10px">RGPV</h2>
-            <h2 className="C">Bhopal</h2>
+          <h2 className="text-white margin-10px">Partners at</h2>
+          <h2 className="G margin-10px">GDSC</h2>
+          <h2 className="D margin-10px">UIT</h2>
+          <h2 className="S margin-10px">RGPV</h2>
+          <h2 className="C">Bhopal</h2>
         </div>
         <div className="cards-list">
           <div className="card">
-            <div className="card_image">
-              <img src={semikolan} />
+            <div className="card_image semikolan">
+              <img src={semikolan} alt="semikolan" />
             </div>
+            <p>
+              <a href="/">semikolan</a>
+            </p>
           </div>
           <div className="card">
             <div className="card_image semikolan">
-              <img src={semikolan} />
+              <img src={semikolan} alt="semikolan" />
             </div>
+            <p>
+              <a href="/">semikolan</a>
+            </p>
+          </div>
+          <div className="card">
+            <div className="card_image semikolan">
+              <img src={semikolan} alt="semikolan" />
+            </div>
+            <p>
+              <a href="/">semikolan</a>
+            </p>
           </div>
           <div className="card">
             <div className="card_image finflee">
-              <img src={finflee} />
+              <img src={finflee} alt="FinFlee" />
             </div>
+            <p>
+              <a href="/">FinFlee</a>
+            </p>
           </div>
         </div>
       </div>

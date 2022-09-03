8eb3779d5215bf596b1989fc0ac2188ce31c56e1
Rajat Bhaskare <80106274+RajatBhaskare7@users.noreply.github.com>
2022-09-04 00:13:27 +0530

    added changes

--- src/Component/Landing.js
+++ src/Component/Landing.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import Logo from '../Assets/img/landing3.png';
+import Logo from '../Assets/img/landing5.png';
 import './css/landing.css';
 export default class Landing extends Component {
   render() {
     return (
       <div className='banner-landing '>
-        <img src={Logo} alt="logo" className='gdsc-landing-logo'/>
+        <img src={Logo} alt="logo" className='gdsc-landing-logo' style={{height:'90%',width:'90%'}}/>
         
                 
       </div>

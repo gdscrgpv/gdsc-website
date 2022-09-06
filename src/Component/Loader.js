f26b95e93c88fd0c549fbbc1d36a985151f0c855
Satyam Sharma <satyamsharma1725@gmail.com>
2022-09-07 01:54:24 +0530

    loader size changed

--- src/Component/Loader.js
+++ src/Component/Loader.js
@@ -5,7 +5,7 @@ export default class
   render() {
     return (
       <div style={{height:'100vh'}} className="d-flex align-items-center justify-content-center">
-        <lottie-player src="https://assets1.lottiefiles.com/packages/lf20_heoa8x46.json"  background="transparent"  speed="1"  style={{width:'50px' ,height:'40px'}}  loop autoplay></lottie-player>
+        <lottie-player src="https://assets1.lottiefiles.com/packages/lf20_heoa8x46.json"  background="transparent"  speed="1"  style={{width:'250px' ,height:'2a00px'}}  loop autoplay></lottie-player>
       </div>
     )
   }

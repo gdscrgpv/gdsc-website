99bf5caf822083981fe49dfb0294899923c066f2
Jay Sanduke <Jaysanduke@gmail.com>
2022-10-04 23:20:36 +0530

    subscribe done

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -2,7 +2,93 @@ import React, { Component } from "react";
 import "../Assets/css/footer.css";
 import GDSC from "../Assets/img/GDSC_Logo.png";
 import { Link } from "react-router-dom";
+
+//Firebase
+import { db } from "../config/firebase";
+import { rtdb } from "../config/firebase";
+import { doc, setDoc, onSnapshot } from "firebase/firestore";
+import { ref, child, push, update, onValue } from "firebase/database";
+
 export default class Footer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.subscribe = this.subscribe.bind(this);
+  }
+
+  handleEmailChange = (event) => {
+    event.preventDefault();
+    this.setState({ email: event.target.value });
+  };
+
+
+  async subscribe(event) {
+    event.preventDefault();
+
+     let docdata = null;
+
+    const emailexist = onSnapshot(doc(db, "subscribers", this.state.email), (doc) => {
+      console.log("Current data: ", doc.data());
+      docdata = doc.data();
+    });
+
+    if (docdata === undefined) {
+      console.log("Email does not exist");
+
+      const newPostKey = push(child(ref(rtdb), 'subscribers')).key;
+
+      await update(ref(rtdb, 'subscribers'), {
+        [newPostKey]: {
+          key: newPostKey,
+          email: this.state.email,
+        },
+      })
+        .then(() => {
+          console.log("Subribed successfully. rtdb");
+        })
+        .catch((error) => {
+          console.log("Error while subscribing: " + error);
+        });
+
+      const docRef = doc(db, "subscribers", this.state.email);
+      await setDoc(docRef, {
+        email: this.state.email,
+        key: newPostKey,
+      })
+        .then(() => {
+          console.log("Subribed successfully. rtdb");
+        })
+        .catch((error) => {
+          console.log("Error: ", error);
+        });
+    }
+    else {
+      console.log("Email already exists");
+
+      await onValue(ref(rtdb, 'subscribers/' + docdata.key), (snapshot) => {
+        const data = snapshot.val();
+
+        if (data && data.email === this.state.email) {
+          console.log("Email already exists in rtdb");
+        }
+        else {
+          update(ref(rtdb, 'subscribers/'), {
+            [docdata.key]: {
+              key: docdata.key,
+              email: this.state.email,
+            }
+          })
+        }
+      });
+    }
+  }
+
+
   render() {
     return (
       <footer className="footer-section">
@@ -83,8 +169,9 @@ export default class Footer extends Component {
                       <input
                         type="text"
                         placeholder="Enter email address here..."
+                        onChange={this.handleEmailChange}
                       />
-                      <button>Subscribe</button>
+                      <button onClick={this.subscribe}>Subscribe</button>
                     </form>
                   </div>
                 </div>

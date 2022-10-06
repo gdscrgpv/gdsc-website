aa6f06846ad86f7eab6e1a5bb41a0b7b915aaa5d
Ujwal Kumar <ujwalkumar95@gmail.com>
2022-10-07 00:20:06 +0530

    Add navbar and footer to 404 error page

--- src/Component/Error404Card.js
+++ src/Component/Error404Card.js
@@ -2,18 +2,19 @@ import { Link } from "react-router-dom";
 import "../Assets/css/error404card.css";
 
 const Error404Card = () => {
-    return (<div className="error-404-main">
+    return (<div className="error-404-main pt-5 pb-5">
             <div className="oops-text">
-                Oops!
+                <span className="G">O</span>
+                <span className="D">o</span>
+                <span className="S">p</span>
+                <span className="C">s</span>
+                <span>!</span>
             </div>
             <div className="page-not-found-text">
                 404 - PAGE NOT FOUND
             </div>
             <div className="page-not-found-tip-text">
-                The page you are looking for might have been removed,
-            </div>
-            <div className="page-not-found-tip-text">
-                had its name changed or is temporarily available.
+                This is not the web page you are looking for.
             </div>
             <button className="go-to-homepage-btn">
                <Link className="link_text font-face-Bold " to="/">

556e70a09f460e1dca751b2e289264b234577f89
Satyam Sharma <satyamsharma1725@gmail.com>
2022-10-21 15:43:42 +0530

    Cards Removed

--- src/Component/EventCard.js
+++ src/Component/EventCard.js
@@ -10,68 +10,6 @@ import Footer from "./Footer";
 import NavBarcomp from "./NavBar";
 
 const EventCard = () => {
-  const EventData = [
-    {
-      id: 1,
-      name: "Android Study Jams",
-      content:
-        " Start your android study jams while earning a branded Hacktoberfest T-shirt.",
-      logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2FH-icon-dark.png?alt=media&token=777c1020-3747-44d5-8f69-acf6ce18e887",
-      banner:
-        "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2Fbanner08-light.png?alt=media&token=b70219af-f468-4139-a707-f9dd9627f2ba",
-      link: "https://gdsc.community.dev/events/details/google-gdsc-guru-gobind-singh-indraprastha-university-presents-android-study-jams/",
-    },
-    {
-      id: 1,
-      name: "Hacktober-fest",
-      content:
-        " Start your android study jams while earning a branded Hacktoberfest T-shirt.",
-      logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2FH-icon-dark.png?alt=media&token=777c1020-3747-44d5-8f69-acf6ce18e887",
-      banner:
-        "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2Fbanner08-light.png?alt=media&token=b70219af-f468-4139-a707-f9dd9627f2ba",
-      link: "https://gdsc.community.dev/events/details/google-gdsc-guru-gobind-singh-indraprastha-university-presents-android-study-jams/",
-    },
-    {
-      id: 1,
-      name: "Hacktober-fest",
-      content:
-        " Start your android study jams while earning a branded Hacktoberfest T-shirt.",
-      logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2FH-icon-dark.png?alt=media&token=777c1020-3747-44d5-8f69-acf6ce18e887",
-      banner:
-        "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2Fbanner08-light.png?alt=media&token=b70219af-f468-4139-a707-f9dd9627f2ba",
-      link: "https://gdsc.community.dev/events/details/google-gdsc-guru-gobind-singh-indraprastha-university-presents-android-study-jams/",
-    },
-    {
-      id: 1,
-      name: "Android Study Jams",
-      content:
-        " Start your android study jams while earning a branded Hacktoberfest T-shirt.",
-      logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2FH-icon-dark.png?alt=media&token=777c1020-3747-44d5-8f69-acf6ce18e887",
-      banner:
-        "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2Fbanner08-light.png?alt=media&token=b70219af-f468-4139-a707-f9dd9627f2ba",
-      link: "https://gdsc.community.dev/events/details/google-gdsc-guru-gobind-singh-indraprastha-university-presents-android-study-jams/",
-    },
-    {
-      id: 1,
-      name: "Hacktober-fest",
-      content:
-        " Start your android study jams while earning a branded Hacktoberfest T-shirt.",
-      logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2FH-icon-dark.png?alt=media&token=777c1020-3747-44d5-8f69-acf6ce18e887",
-      banner:
-        "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2Fbanner08-light.png?alt=media&token=b70219af-f468-4139-a707-f9dd9627f2ba",
-      link: "https://gdsc.community.dev/events/details/google-gdsc-guru-gobind-singh-indraprastha-university-presents-android-study-jams/",
-    },
-    {
-      id: 1,
-      name: "Hacktober-fest",
-      content:
-        " Start your android study jams while earning a branded Hacktoberfest T-shirt.",
-      logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2FH-icon-dark.png?alt=media&token=777c1020-3747-44d5-8f69-acf6ce18e887",
-      banner:
-        "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2Fbanner08-light.png?alt=media&token=b70219af-f468-4139-a707-f9dd9627f2ba",
-      link: "https://gdsc.community.dev/events/details/google-gdsc-guru-gobind-singh-indraprastha-university-presents-android-study-jams/",
-    },
-  ];
 
   return (
     <>
@@ -221,65 +159,6 @@ const EventCard = () => {
                 <img src={opensource} className="event-image" />
               </div>
             </div>
-          </div>
-        </div>
-
-
-        <div className="session-cards">
-          <div className="event-section-title">Sessions</div>
-          <div class="wrap-event">
-            {EventData.map((event) => {
-              return (
-                <article class="card-event">
-                  <div class="card-inner-event">
-                    <div class="card-image-box">
-                      <img
-                        src="https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2Fbanner08-light.png?alt=media&token=b70219af-f468-4139-a707-f9dd9627f2ba"
-                        alt=""
-                        width="330"
-                        class="card-event-image"
-                      />
-                    </div>
-
-                    <div class="card-text-box">
-                      <div class="card-event-flex-box">
-                        <img
-                          src="https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2FH-icon-dark.png?alt=media&token=777c1020-3747-44d5-8f69-acf6ce18e887"
-                          alt=""
-                          class="card-event-logo"
-                        />
-
-                        <p
-                          style={{
-                            fontSize: 20,
-                            fontWeight: "550",
-                            marginLeft: "10%",
-                          }}
-                          class="card-event-text"
-                        >
-                          {event.name}
-                        </p>
-                      </div>
-
-                      <div class="card-event-column-box">
-                        <p
-                          style={{ fontSize: 14, fontWeight: "500" }}
-                          class="card-event-text"
-                        >
-                          {event.content}
-                        </p>
-                      </div>
-
-                      <button class="know-more-btn-small">
-                        <a className="link-event" href={event.link}>
-                          Know More
-                        </a>
-                      </button>
-                    </div>
-                  </div>
-                </article>
-              );
-            })}
           </div>
         </div>
         <Footer />

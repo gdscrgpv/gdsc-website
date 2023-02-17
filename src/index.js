318427982245959e5597dfbd15f23b5f632906a9
Satyam Sharma <satyamsharma1725@gmail.com>
2023-02-17 13:42:39 +0530

    uxmeetup removed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import Gallery from "./Pages/Gallery";
 import Contact from "./Pages/Contact";
 import Event from "./Pages/Event";
 import Error404 from "./Pages/Error404";
-import UXMeetup from "./Pages/UXMeetup";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
@@ -21,7 +20,6 @@ root.render(
       <Route path="/gallery" element={<Gallery />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/event" element={<Event />} />
-      <Route path="/uxmeetup" element={<UXMeetup />} />
       <Route path="*" element={<Error404 />} />
     </Routes>
   </BrowserRouter>
